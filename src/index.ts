4f336fd45c1ef624e6599f096b587b04108aceda
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-07-03 17:34:06 +0200

    export everythin :imp:

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
-export { Container, DIBadKeyError } from './container';
-export { DIAggregateError, DIError } from './errors';
+
+export * from './container';
+export * from './errors';
 export * from './decorators'
 export * from './activators';
 export * from './resolvers';
